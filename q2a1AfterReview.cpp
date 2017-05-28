cb4fbeb21b11c659b9899ba2b8b384c25212d9cd
Marilena Bescuca <mary_bescuca@yahoo.com>
2017-05-28 22:28:00 +0100

    Answer To Assignment1, Q2 - Changes After Review

---
+++ q2a1AfterReview.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+#include <vector>
+#define MAX_DIM 100000
+
+using namespace std;
+
+struct node {
+    int value;
+    node *next;
+};
+
+node *head;
+
+node* arrayToList(vector <int> a){
+    //this method converts the vector a to a singly linked list
+    //use a dummy header and iter, which points to the last element in the list at a given point
+    node *start = new node;
+    node *iter = start;
+    for(int i = 0; i < a.size(); ++i){
+        node *temp = new node;
+        temp -> value = a[i];
+        temp -> next = NULL;
+        iter -> next = temp;
+        iter = iter -> next;
+    }
+    return start -> next;
+}
+
+node* kthToLast(node *start, int k){
+    //this method returns NULL if the list has less than k + 1 elements, otherwise it returns the kth to last node from the list
+    int i = 1;
+    node *iter2 = start, *iter1 = start;
+    //use two pointers, firstly move the second one kth positions down the list from the start
+    //and then move the two pointers simultaneously
+    //when the second one (iter) gets to the end of the list, the first one (start) is at the kth to last element
+    while(i <= k && iter2 != NULL){
+        iter2 = iter2 -> next;
+        i++;
+    }
+    if(i <= k) {
+        return NULL;
+    }
+    while(iter2 -> next != NULL){
+        iter2 = iter2 -> next;
+        iter1 = iter1 -> next;
+    }
+    return iter1;
+}
+
+int main()
+{
+    vector <int> a;
+    int k, number;
+    cin>>k;
+    //read input into an array
+    while(cin >> number){
+        a.push_back(number);
+    }
+    //convert the array into a singly linked list
+    head = arrayToList(a);
+
+    node *kthToLastNode = kthToLast(head, k);
+    if (kthToLastNode != NULL) {
+        cout << kthToLastNode -> value << "\n";
+    }
+    else {
+        cout << "The list has less than k + 1 elements.\n";
+    }
+    return 0;
+}
